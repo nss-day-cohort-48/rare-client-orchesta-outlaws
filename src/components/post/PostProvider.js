107a4564aea38644fa5d1da1decc7d7ef3ae6335
Madeline Crowley <madelinecrowley@gmail.com>
2021-07-27 15:01:40 -0500

    added PostProvider

---
+++ src/components/post/PostProvider.js
@@ -0,0 +1,19 @@
+import React, { createContext } from "react"
+
+export const PostContext = createContext()
+
+export const PostProvider = (props) => {
+
+    const getUserPosts = (id) => {
+        return fetch(`http://localhost:8088/posts?user_id=${id}`)
+        .then(res => res.json())
+    }
+
+    return (
+        <PostContext.Provider value={{
+            getUserById
+        }}>
+            {props.children}
+        </PostContext.Provider>
+    )
+}
