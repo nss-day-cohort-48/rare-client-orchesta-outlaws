813b111f0ffb93e5d8d6ba1745bca170d8a3c44e
Val Currie <contact@valcuri.email>
2021-07-30 10:25:16 -0500

    factored out Madelines dateConvert function

--- src/components/utils/HumanDate.js
+++ src/components/utils/HumanDate.js
@@ -1,10 +1,17 @@
 export const HumanDate = ({ date }) => {
-    return new Date(date.replace(/-/g, '\/')).toLocaleDateString("en-US",
-    {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        timeZone: 'America/Chicago'
-    })
-}
+  return new Date(date.replace(/-/g, "/")).toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: "America/Chicago",
+  });
+};
+
+export const dateConvert = (ISOdate) => {
+  const date = ISOdate.split("-");
+  const year = date.shift();
+  date[0] = `${date[0]}/`;
+  date.push(`/${year}`);
+  return date.join("");
+};
