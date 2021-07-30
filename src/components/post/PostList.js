c80a1c5a67762040ab1d045d185dba04bbd1d230
Val Currie <contact@valcuri.email>
2021-07-30 10:26:26 -0500

    finished the homepage view (SubbedPosts)

--- src/components/post/PostList.js
+++ src/components/post/PostList.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { dateConvert } from "../utils/HumanDate";
+import { BsFillGearFill } from "react-icons/bs";
+import { FaTrashAlt } from "react-icons/fa";
+import { Button, Modal } from "react-bootstrap";
+import "./Post.css";
 
-export const PostList = ({ postsArray, postReactions, author }) => (
-  <div className="posts">
-    {postsArray.map((p) => {
-      return (
-        <div key="p.id" className="post">
+export const PostList = ({ postsArray, postReactions, author }) => {
+  const history = useHistory();
+  const [modalShow, setModalShow] = useState(false);
+  const MyVerticallyCenteredModal = (props) => {
+    return (
+      <Modal
+        className="modal"
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header className="modal_header" closeButton></Modal.Header>
+        <Modal.Body>
+          <p>Are you sure you want to delete this post?</p>
+        </Modal.Body>
+        <Modal.Footer className="modal_footer">
+          <Button onClick={props.onHide}>Yes</Button>
+          <Button onClick={props.onHide}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+  return (
+    <div className="posts">
+      {postsArray.map((p) => (
+        <div key={p.id} className="post">
           <div className="post__header">
             <div className="post__title">
               <Link to={`/posts/detail/${p.id}`}>
@@ -33,7 +61,7 @@ export const PostList = ({ postsArray, postReactions, author }) => (
             <div className="post__reaction-count">
               {postReactions.length > 0
                 ? postReactions.filter((pr) => pr.post_id === p.id).length
-                : 0}
+                : 0}{" "}
               Reactions
             </div>
             {author && (
@@ -62,7 +90,7 @@ export const PostList = ({ postsArray, postReactions, author }) => (
             />
           </div>
         </div>
-      );
-    })}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
--- src/components/post/SubbedPosts.js
+++ src/components/post/SubbedPosts.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { PostReactionContext } from "../postReaction/PostReactionProvdier";
+import { PostList } from "./PostList";
+import { PostContext } from "./PostProvider";
 
 export const SubbedPosts = (props) => {
-  return "I am your subbed posts! :)";
+  const [posts, setPosts] = useState([]);
+  const { getUserSubbedPosts } = useContext(PostContext);
+  const { postReactions, getPostReactions } = useContext(PostReactionContext);
+  useEffect(() => {
+    getUserSubbedPosts(localStorage.getItem("rare_user_id"))
+      .then(setPosts)
+      .then(() => getPostReactions());
+  }, []);
+  return <PostList postsArray={posts} postReactions={postReactions} />;
 };
