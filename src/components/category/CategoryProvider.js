a2b489b9a44c962a91f6456006c67fd828288522
alambrow <alexander.lambrow@gmail.com>
2021-08-16 15:52:08 -0500

    changed fetch calls for cat info

--- src/components/category/CategoryProvider.js
+++ src/components/category/CategoryProvider.js
@@ -6,16 +6,21 @@ export const CategoryProvider = (props) => {
     const [categories, setCategories] = useState([])
 
     const getAllCategories = () => {
-        return fetch("http://localhost:8088/categories")
+        return fetch("http://localhost:8000/categories", {
+            headers: {
+                "Authorization": "Token b0935533e22f91a6437fdbc3f2f54b778f349fd5"
+            }
+        })
         .then(res => res.json())
         .then(setCategories)
     }
 
     const addCategory = catObj => {
-        return fetch("http://localhost:8088/categories", {
+        return fetch("http://localhost:8000/categories", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
             },
             body: JSON.stringify(catObj)
         })
@@ -23,10 +28,11 @@ export const CategoryProvider = (props) => {
     }
 
     const updateCategory = catObj => {
-        return fetch(`http://localhost:8088/categories/${catObj.id}`, {
+        return fetch(`http://localhost:8000/categories/${catObj.id}`, {
             method: "PUT",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
             },
             body: JSON.stringify(catObj)
         })
@@ -34,8 +40,11 @@ export const CategoryProvider = (props) => {
     }
 
     const deleteCategory = catId => {
-        return fetch(`http://localhost:8088/categories/${catId}`, {
-            method: "DELETE"
+        return fetch(`http://localhost:8000/categories/${catId}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            }
         })
         .then(getAllCategories)
     }
