edd2ea0a5c0342eb07fb6f647bd4696ac058447f
Val Currie <contact@va.lcuri.email>
2021-08-17 15:11:21 -0500

    mild styling

--- src/components/post/AllPosts.js
+++ src/components/post/AllPosts.js
@@ -3,6 +3,8 @@ import { PostList } from "./PostList";
 import { PostContext } from "./PostProvider";
 import { PostReactionContext } from "../postReaction/PostReactionProvdier";
 import { Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Post.css";
 
 export const AllPosts = (props) => {
   const [posts, setPosts] = useState([]);
@@ -12,25 +14,27 @@ export const AllPosts = (props) => {
     getAllPosts().then(setPosts);
   }, []);
   return (
-    <Table bordered>
-      <thead>
-        <th>Title</th>
-        <th>Author</th>
-        <th>Date</th>
-        <th>Category</th>
-        <th>Tags</th>
-      </thead>
-      {posts.map((p) => (
-        <tr key={p.id}>
-          <td>{p.title}</td>
-          <td>
-            {p.rare_user.user.first_name + " " + p.rare_user.user.last_name}
-          </td>
-          <td>{p.publication_date}</td>
-          <td>{p.category.label}</td>
-          <td>TAGS</td>
-        </tr>
-      ))}
-    </Table>
+    <div className="allposts__container">
+      <Table bordered className="allposts__table">
+        <thead>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Date</th>
+          <th>Category</th>
+          <th>Tags</th>
+        </thead>
+        {posts.map((p) => (
+          <tr key={p.id}>
+            <td>{p.title}</td>
+            <td>
+              {p.rare_user.user.first_name + " " + p.rare_user.user.last_name}
+            </td>
+            <td>{p.publication_date}</td>
+            <td>{p.category.label}</td>
+            <td>TAGS</td>
+          </tr>
+        ))}
+      </Table>
+    </div>
   );
 };
