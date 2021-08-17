0b536db1092e9e6941cec503175e65151eb6ebe8
Val Currie <contact@va.lcuri.email>
2021-08-17 14:45:41 -0500

    added a react-bootstrap table to AllPosts

--- src/components/post/AllPosts.js
+++ src/components/post/AllPosts.js
@@ -2,15 +2,18 @@ import React, { useState, useContext, useEffect } from "react";
 import { PostList } from "./PostList";
 import { PostContext } from "./PostProvider";
 import { PostReactionContext } from "../postReaction/PostReactionProvdier";
+import { Table } from "react-bootstrap";
 
 export const AllPosts = (props) => {
   const [posts, setPosts] = useState([]);
   const { getAllPosts } = useContext(PostContext);
   const { postReactions, getPostReactions } = useContext(PostReactionContext);
   useEffect(() => {
-    getAllPosts()
-      .then(setPosts)
-      .then(() => getPostReactions());
+    getAllPosts().then(setPosts);
   }, []);
-  return <PostList postsArray={posts} postReactions={postReactions} />;
+  return (
+    <Table bordered>
+      <thead></thead>
+    </Table>
+  );
 };
