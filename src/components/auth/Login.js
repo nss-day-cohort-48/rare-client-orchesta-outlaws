2730d40d3ef28c5051cef61a897bf6bd9e1efca3
Val Currie <contact@va.lcuri.email>
2021-08-17 10:27:53 -0500

    Revert "Started standardizing auth/api stuff"
    This reverts commit df890b62941400873a51f0aada58ec5999eb388e.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { login } from "../../utils/auth";
 import "./Auth.css";
 
 export const Login = () => {
@@ -11,14 +10,27 @@ export const Login = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    login(email.current.value, password.current.value).then((res) => {
-      if (res.id) {
-        localStorage.setItem("rare_user_id", res.id);
-        history.push("/");
-      } else {
-        window.alert("Email or password was not valid.");
-      }
-    });
+
+    return fetch("http://127.0.0.1:8088/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        email: email.current.value,
+        password: password.current.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.id) {
+          localStorage.setItem("rare_user_id", res.id);
+          history.push("/");
+        } else {
+          window.alert("Email or password was not valid.");
+        }
+      });
   };
 
   return (
--- src/utils/api.js
+++
@@ -1,3 +0,0 @@
-const apiHost = "127.0.0.1";
-const apiPort = "8000";
-export const apiURL = "http://" + apiHost + ":" + apiPort;
