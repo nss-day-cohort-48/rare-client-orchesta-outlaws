77b5ad4f875e9f6b6142d89807fdc71f9ae4f87b
Val Currie <contact@va.lcuri.email>
2021-08-18 10:32:43 -0500

    refactored ReactionProvider using authFetch and apiURL from utils

--- src/components/reaction/ReactionProvider.js
+++ src/components/reaction/ReactionProvider.js
@@ -1,45 +1,39 @@
 import React, { createContext, useState } from "react";
+import { authFetch } from "../../utils/auth";
+import { apiURL } from "../../utils/api";
 
-export const ReactionContext = createContext()
+export const ReactionContext = createContext();
 
 export const ReactionProvider = (props) => {
-    const [reactions, setReactions] = useState([])
+  const [reactions, setReactions] = useState([]);
 
-    const getAllReactions = () => {
-        return fetch("http://localhost:8000/reactions", {
-            headers: {
-                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
-            }
-        })
-        .then(res => res.json())
-        .then(setReactions)
-    }
+  const getAllReactions = () => {
+    return authFetch(`${apiURL}/reactions`)
+      .then((res) => res.json())
+      .then(setReactions);
+  };
 
-    const deleteReaction = reactionId => {
-        return fetch(`http://localhost:8000/reactions/${reactionId}`, {
-            method: "DELETE",
-            headers: {
-                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
-            }
-        })
-        .then(getAllReactions)
-    }
+  const deleteReaction = (reactionId) => {
+    return authFetch(`${apiURL}/reactions/${reactionId}`, {
+      method: "DELETE",
+    }).then(getAllReactions);
+  };
 
-    const createReaction = reactObj => {
-        return fetch("http://localhost:8000/reactions", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
-            },
-            body: JSON.stringify(reactObj)
-        })
-        .then(getAllReactions)
-    }
+  const createReaction = (reactObj) => {
+    return fetch(`${apiURL}/reactions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reactObj),
+    }).then(getAllReactions);
+  };
 
-    return (
-        <ReactionContext.Provider value={{reactions, getAllReactions, deleteReaction, createReaction}}>
-            {props.children}
-        </ReactionContext.Provider>
-    )
-}
+  return (
+    <ReactionContext.Provider
+      value={{ reactions, getAllReactions, deleteReaction, createReaction }}
+    >
+      {props.children}
+    </ReactionContext.Provider>
+  );
+};
