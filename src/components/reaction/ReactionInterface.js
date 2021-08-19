0fcc9ce8aa94d3f957702a70f1a8e0c2474cae3e
alambrow <alexander.lambrow@gmail.com>
2021-08-19 14:35:17 -0500

    still working

--- src/components/reaction/ReactionInterface.js
+++ src/components/reaction/ReactionInterface.js
@@ -3,27 +3,6 @@ import { PostContext } from '../post/PostProvider';
 import { ReactionContext } from "./ReactionProvider";
 import "./reaction_interface.css";
 
-const post = [{
-    "id": 1,
-    "rare_user": {
-        "id": 1,
-        "user": {
-            "first_name": "Steve",
-            "last_name": "Brownlee",
-            "username": "me@me.com"
-        }
-    },
-    "category": {
-        "id": 1,
-        "label": "News"
-    },
-    "title": "Cassowary",
-    "publication_date": "2021-07-07",
-    "image_url": "https://www.guinnessworldrecords.com/Images/cassowary-side_tcm25-568936.jpg",
-    "content": "cool bird",
-    "approved": true,
-    "isMine": true
-}]
 
 export const ReactionInterface = (post) => {
     const { reactions, getAllReactions} = useContext(ReactionContext)
